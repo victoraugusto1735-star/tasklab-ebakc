37e095e75844089d719553d1d1bb321a922be335
victor1735 <victoraugusto7311@gmail.com>
2026-05-08 18:30:40 -0300

    fix task type in hook test

--- tests/useContadorDeTarefas.test.ts
+++ tests/useContadorDeTarefas.test.ts
@@ -1,17 +1,17 @@
 import { renderHook } from "@testing-library/react";
 import { useContadorDeTarefas } from "../hooks/useContadorDeTarefas";
 
-describe('useContadorDeTarefas', () => {
-    it('deve retornar o número total de tarefas', () => {
-        const tarefas = [
-            { id: 1, texto: 'Tarefas 1' },
-            { id: 2, texto: 'Tarefas 2' },
-        ]
+describe("useContadorDeTarefas", () => {
+  it("deve retornar o número total de tarefas", () => {
+    const tarefas = [
+      { id: 1, texto: "Tarefa 1", concluida: false },
+      { id: 2, texto: "Tarefa 2", concluida: true },
+    ];
 
-        const { result } = renderHook(() => 
-            useContadorDeTarefas(tarefas)
-        )
+    const { result } = renderHook(() =>
+      useContadorDeTarefas(tarefas)
+    );
 
-        expect(result.current).toBe(2)
-    })
-})
+    expect(result.current).toBe(2);
+  });
+});
